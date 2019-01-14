b6c53b4574bcce3058dd5dd8c53df03ad452d2ea
saigowthamr <saigowthamr@gmail.com>
2019-01-14 08:37:25 +0530

    tim

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,22 @@
 const path = require("path")
 const { createFilePath, createFileNode } = require(`gatsby-source-filesystem`)
 
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth() + 1; //January is 0!
+
+var yyyy = today.getFullYear();
+if (dd < 10) {
+    dd = '0' + dd;
+}
+if (mm < 10) {
+    mm = '0' + mm;
+}
+var today = yyyy + '/' + mm + '/' + dd
+
+console.log(today)
+
+
 exports.createPages = ({ actions, graphql }) => {
     const { createPage } = actions
 
